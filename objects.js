762d5f7eb87056037742149416864089bb5d2379
Jay Kirtlan <jaykirtlan@hotmail.com>
2023-07-22 18:31:28 +0100

    created object functions

---
+++ objects.js
@@ -0,0 +1,15 @@
+// function that merges two objects
+function mergeObjects(obj1, obj2) {
+    return { ...obj1, ...obj2 };
+}
+
+
+// takes an object and returns true if the object is emptyCells: 
+function isPersonEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+
+// takes object as input and returns an array of its keys 
+function getObjectKeys(obj) {
+    return Object.keys(obj);
+}
